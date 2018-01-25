a4832ccef31bea7305dc528906583d4f4f9afa00
Nouru Muneza <nizzogentl@gmail.com>
2018-01-25 11:54:27 -0800

    Update Solution.cpp

--- Solution.cpp
+++ Solution.cpp
@@ -1,1 +1,38 @@
+int Solution::coverPoints(vector<int> &A, vector<int> &B) {
+   int steps = 0, dx, dy, i = 0;
+
+    if(A.size() <=1 || B.size() <= 1 || A.size() != B.size()){
+        return 0;
+    }
+
+    do{
+        dx = A[i] - A[i+1];
+        dy = B[i] - B[i+1];
+
+        if(dx < 0){
+            dx = -dx;
+        }
+        if(dy < 0){
+            dy = -dy;
+        }
+
+        while( dx != 0 && dy != 0 ){
+            dx--;
+            dy--;
+            steps++;
+        }
+        if(dx == 0){
+            steps = steps + dy;
+        }
+        else{
+            steps = steps + dx;
+        }
+        i++;
+    }while(i < A.size()-1);
+
+    return steps;
+
+}
+    
+
 
